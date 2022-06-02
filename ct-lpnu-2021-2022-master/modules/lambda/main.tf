5995c00fe845e4fb359b9818719651ba9c80698f
Garber1329 <91221454+Garber1329@users.noreply.github.com>
2022-06-02 15:31:13 +0300

    first commit

---
+++ ct-lpnu-2021-2022-master/modules/lambda/main.tf
@@ -0,0 +1,105 @@
+locals {
+  log_retention = 3
+}
+
+module "label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  context = var.context
+  name    = var.name
+}
+
+module "label_get_all_authors" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  context = var.context
+  name    = "get-all-authors"
+}
+
+module "label_get_all_courses" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  context = var.context
+  name    = "get-all-courses"
+}
+
+data "aws_region" "current" {}
+
+module "lambda_function_get_all_authors" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-get-all-authors"
+  function_name                     = module.label_get_all_authors.id
+  description                       = "get all authors"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.get_all_authors_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+  source_path = "${path.module}/lambda_source/get_all_authors/index.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_authors_name
+  }
+
+  tags = module.label.tags
+  depends_on = [
+    aws_cloudwatch_log_group.get_all_authors
+  ]
+}
+
+module "lambda_function_get_all_courses" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-get-all-courses"
+  function_name                     = module.label_get_all_courses.id
+  description                       = "get all courses"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.get_all_courses_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+
+  source_path = "${path.module}/lambda_source/get_all_courses/index.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_courses_name
+  }
+
+  tags = module.label.tags
+  depends_on = [
+    aws_cloudwatch_log_group.get_all_courses
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "get_all_authors" {
+  name              = format("/aws/lambda/%s", module.label_get_all_authors.id)
+  retention_in_days = local.log_retention
+
+  # tags = merge(var.tags,
+  #   { Function = format("%s", module.label_get_all_authors.id) }
+  # )
+}
+
+resource "aws_cloudwatch_log_group" "get_all_courses" {
+  name              = format("/aws/lambda/%s", module.label_get_all_courses.id)
+  retention_in_days = local.log_retention
+
+  # tags = merge(var.tags,
+  #   { Function = format("%s", module.label_get_all_courses.id) }
+  # )
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda_function_get_all_authors.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${var.aws_lambda_permission_api_gateway_source_arn}/*${var.aws_api_gateway_resource_authors_path}"
+}
